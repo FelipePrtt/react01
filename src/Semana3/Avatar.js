2d7951568fea154c2a9e1662a3cc1dbd6c47f576
Felipe Pretti <felipepretti2001@gmail.com>
2025-05-27 00:15:25 -0300

    Checkpoint - Slide 22

--- src/Semana3/Avatar.js
+++ src/Semana3/Avatar.js
@@ -1,20 +1,13 @@
-function Avatar({person, size}){
+import { getImageUrl } from "./utils";
+
+export default function Avatar({person, size}){
     return(
         <img 
             className="avatar"
-            src="https://upload.wikimedia.org/wikipedia/en/c/cd/Lin_Lanying.jpeg"
-            alt="Lin Lanying"
-            width={100}
-            height={100}
-        />
-    );
-}
-
-export default function Profile(){
-    return(
-        <Avatar 
-            person={{ name: 'Lin Lanying', imageId: '1bX5QH6' }}
-            size={100}
+            src={getImageUrl(person)}
+            alt={person.name}
+            width={size}
+            height={size}
         />
     );
 }
